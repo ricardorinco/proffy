d9f29abd3b5370a0a708c22f9c0768ecb89e395c
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-08-04 14:44:40 -0300

    Sistema de rotas
    Implementação do sistema de roteamento do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { TeacherListComponent } from './pages/teacher/teacher-list/teacher-list.component';
+import { TeacherFormComponent } from './pages/teacher/teacher-form/teacher-form.component';
 
 @NgModule({
-    declarations: [AppComponent, LandingComponent],
-    imports: [BrowserModule],
+    declarations: [
+        AppComponent,
+        LandingComponent,
+        TeacherListComponent,
+        TeacherFormComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
