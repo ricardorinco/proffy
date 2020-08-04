7b8265f59436e9ccaf530f9c6f1d5452449568ee
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-08-04 14:40:59 -0300

    Criação da Landing Page
    Importação dos assets, criação da página de Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, LandingComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent]
